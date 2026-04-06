cb52a8f55485c1d756add26842091224ff866928
Austin Vu <freeluck.v.i.p999@gmail.com>
2026-04-05 22:49:53 -0500

    start of my generational run loop for the algo

--- src/main-ga.cpp
+++ src/main-ga.cpp
@@ -16,63 +16,33 @@ int numRooms = rooms.size();
 int popSize = 100;
 
 // population variable in main
-std::vector<ChromosomeStruct> mainpop = generatePopulation(popSize, numClasses, numRooms, rooms, classes);  
-std::cout << "Generated " << mainpop.size() << " chromosomes, each with " << mainpop[0].chromosome.size() << " genes." << std::endl;
-std::cout << "Fitness: " << mainpop[0].fitness << ", H= " << computeH(mainpop[0].chromosome, classes, rooms) << std::endl;
+std::vector<ChromosomeStruct> mainpop = generatePopulation(popSize, numClasses, numRooms, rooms, classes);
+  
+//inital setup ends here
+//start of loop
+// vector to hold the populations
+std::vector<std::vector<ChromosomeStruct>> GenerationalRun; 
 
+// 500 generations in the making
+for(int i = 0; i < 500; i++){
 //passing vector struct mainpop to elite
 std::vector<double> elitechromo = elite(mainpop);
-//displaying elite array
-std::cout << "Elite Array:\n";
-for(size_t i = 0; i < elitechromo.size(); i++){
-    std::cout << elitechromo[i] << std::endl;
+//2 parents breeding each time so we do it 50 times so we get 100 kids
+for(int j = 0; j < 50; j++){ 
+int idx1 = tournamentSelection(mainpop, 5); //tournament selection
+int idx2 = tournamentSelection(mainpop, 5); //tournament selection
+ChromosomeStruct parent1 = mainpop[idx1];
+ChromosomeStruct parent2 = mainpop[idx2];
+ChromosomeStruct child1, child2;
+Children youngsters = crossover(parent1.chromosome, parent2.chromosome); //crossover happens here
+child1.chromosome = youngsters.child1;  // copy youngster struct and map it to the child1 and child2 struct that was made above
+child2.chromosome = youngsters.child2;
+child1.fitness = computeFitness(child1.chromosome, classes, rooms);  // compute fitness for child1 and child2
+child2.fitness = computeFitness(child2.chromosome, classes, rooms);
 }
 
-// Test tournament selection
-std::cout << "\n--- Testing Tournament Selection ---" << std::endl;
-
-// Perform 3 tournament selections to demonstrate
-//Uncomment code below to test tournament selection
-for (int t = 0; t < 3; t++) {
-    int selectedIdx = tournamentSelection(mainpop, 5);
-    std::cout << "Tournament " << (t+1) << " selected index " << selectedIdx 
-              << " with fitness " << mainpop[selectedIdx].fitness 
-              << ", H= " << computeH(mainpop[selectedIdx].chromosome, classes, rooms) << std::endl;
 }
 
-//testing crossover
-// Uncomment the below code to test the crossover function
- std::cout << "\n--- Testing Crossover ---" << std::endl;
- ChromosomeStruct parent1 = mainpop[tournamentSelection(mainpop, 5)];
- ChromosomeStruct parent2 = mainpop[tournamentSelection(mainpop, 5)];
- ChromosomeStruct child1, child2;
- Children youngsters = crossover(parent1.chromosome, parent2.chromosome);
- child1.chromosome = youngsters.child1;  // copy youngster struct and map it to the child1 and child2 struct that was made above
- child2.chromosome = youngsters.child2;
- child1.fitness = computeFitness(child1.chromosome, classes, rooms);
- child2.fitness = computeFitness(child2.chromosome, classes, rooms);
-/*
- std::cout << "Parent 1: " << std::endl;
- for (int i = 0; i < parent1.chromosome.size(); i++) {
-     std::cout << parent1.chromosome[i] << " ";
- }
- std::cout << std::endl;
- std::cout << "Parent 2: " << std::endl;
- for (int i = 0; i < parent2.chromosome.size(); i++) {
-     std::cout << parent2.chromosome[i] << " ";
- }
- std::cout << std::endl;
- std::cout << "Child1: " << std::endl;
- for (int i = 0; i < child1.chromosome.size(); i++) {
-     std::cout << child1.chromosome[i] << " ";
- }
- std::cout << std::endl;
- std::cout << "Child2: " << std::endl;
- for (int i = 0; i < child2.chromosome.size(); i++) {
-     std::cout << child2.chromosome[i] << " ";
-}
-std::cout << std::endl;
-*/
 
 return 0;
 }
